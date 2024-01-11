a18d70b57c9db6c3994bf09c1e97fa71451fcb27
isawzz <iwgajam@gmail.com>
2024-01-11 11:09:20 -0800

    das

--- combu/start.js
+++ combu/start.js
@@ -1,6 +1,6 @@
 onload = start;
 
-async function start() { test61_lines(); } 
+async function start() { test69_event(); } 
 
 //#region nations tests
 async function natCardsManual() {
@@ -8,9 +8,9 @@ async function natCardsManual() {
 	let dParentGood = toElem('dExtra');
 	let dParentBad = toElem('dPageTitle');
 	DA.eimg = await imgAsync(dParentBad, {}, { src: '../assets/games/nations/empty_inner_card.png', tag: 'img' });
-	let di = { antikythera_mechanism:'left',uluru:null,mount_kailash:null,terracotta_army: 'top', uraniborg: 'left', great_barrier_reef: 'right', hawaii:'left' };
+	let di = {second_boer_war:'right',opium_war:'right',balkan_wars:'right', antikythera_mechanism:'left',uluru:null,mount_kailash:null,terracotta_army: 'top', uraniborg: 'left', great_barrier_reef: 'right', hawaii:'left' };
 	let list=Object.keys(di);
-	list=['antikythera_mechanism']; // all done!
+	list=['second_boer_war','opium_war','balkan_wars']; // all done!
 	for (const k of list) {
 		let card = M.natCards[k];
 		let path = `../assets/games/nations/cards/${card.Path}`;
@@ -31,10 +31,13 @@ async function natCardsManual() {
 		ctx11.drawImage(img, 0, 0, wImg, hImg, 0, 0, wCanvas, hCanvas);
 
 		let noside = di[k];
-		let left = noside == 'left' ? 0 : findLeftLine(ctx1, wCanvas, hCanvas, cgoal); console.log('left', left)
+
+		let xStart = type == 'war'?20:0;
+
+		let left = noside == 'left' ? 0 : findLeftLine(ctx1, wCanvas, hCanvas, cgoal,xStart); console.log('left', left)
 		let right = noside == 'right' ? wCanvas : findRightLine(ctx1, wCanvas, hCanvas, cgoal); console.log('right', right)
 		let top = noside == 'top' ? 0 : findTopLine(ctx1, wCanvas, hCanvas, cgoal); console.log('top', top)
-		let bot = noside == 'bottom' ? hCanvas : findBottomLine(ctx1, wCanvas, hCanvas, cgoal); console.log('bot', bot)
+		let bot = noside == 'bottom'||type=='war' ? hCanvas : findBottomLine(ctx1, wCanvas, hCanvas, cgoal); console.log('bot', bot)
 		let [x1, x2, y1, y2, dx, dy, factw, facth] = [left, right, top, bot, 8, 8, 2, 2];
 
 		if (k == 'hawaii') {dx=16; factw=1.2}
@@ -47,8 +50,78 @@ async function natCardsManual() {
 		await imgToServer(canvas, `y/nat/${type}/${k}.png`);
 
 	}
-	function findLeftLine(ct, w, h, cgoal) {
-		let [restlist, _] = findPointsBoth(ct, 0, 40, 0, h, cgoal, 20);
+	function findLeftLine(ct, w, h, cgoal, xStart=0) {
+
+		let [restlist, _] = findPointsBoth(ct, xStart, xStart+40, 0, h, cgoal, 20);
+		let o = nextBar(ct, restlist, 'red');
+		return o.val;
+	}
+	function findRightLine(ct, w, h, cgoal) {
+		let [restlist, _] = findPointsBoth(ct, w - 40, w, 0, h, cgoal, 20);
+		let o = nextBar(ct, restlist, 'orange');
+		return o.val;
+	}
+	function findTopLine(ct, w, h, cgoal) {
+		let [_, restlist] = findPointsBoth(ct, 0, w, 0, 40, cgoal, 20);
+		let o = nextLine(ct, restlist, 'blue');
+		return o.val;
+	}
+	function findBottomLine(ct, w, h, cgoal) {
+		let [_, restlist] = findPointsBoth(ct, 0, w, h - 30, h, cgoal, 20);
+		let o = nextLine(ct, restlist, 'green');
+		return o.val;
+	}
+}
+async function test69_event() {
+	M.natCards = await mGetYaml('../assets/games/nations/cards.yaml'); return;
+	let dParentGood = toElem('dExtra');
+	let dParentBad = toElem('dPageTitle');
+	DA.eimg = await imgAsync(dParentBad, {}, { src: '../assets/games/nations/empty_inner_card.png', tag: 'img' });
+	let di = {second_boer_war:'right',opium_war:'right',balkan_wars:'right', antikythera_mechanism:'left',uluru:null,mount_kailash:null,terracotta_army: 'top', uraniborg: 'left', great_barrier_reef: 'right', hawaii:'left' };
+	let list=Object.keys(di);
+	list=['second_boer_war','opium_war','balkan_wars']; // all done!
+	for (const k of list) {
+		let card = M.natCards[k];
+		let path = `../assets/games/nations/cards/${card.Path}`;
+		let type = card.Type;
+		let img = await imgAsync(dParentBad, {}, { src: path, tag: 'img' })
+		let [wImg, hImg] = [img.width, img.height]; //console.log('w', w, 'h', h);
+		let [cgoal, clight, lighting] = type == 'event' ? ['#6C4F64', '#E7BB97', false] : ['#59544E', '#DBCEBE', true];
+		let [wCanvas, hCanvas] = [wImg, hImg];
+		let cv1 = mDom(dParentBad, {}, { tag: 'canvas', width: wCanvas, height: hCanvas });
+		let ctx1 = cv1.getContext('2d', { willReadFrequently: true });
+		ctx1.drawImage(img, 0, 0, wImg, hImg, 0, 0, wCanvas, hCanvas);
+
+		console.log('____________', k)
+		let [canvas, ctx, w, h] = await natGetEmptyCardCanvas(dParentGood);
+		mDom(dParentGood, { h: 10 });
+		let cv11 = mDom(dParentBad, {}, { tag: 'canvas', width: wCanvas, height: hCanvas });
+		let ctx11 = cv11.getContext('2d', { willReadFrequently: true });
+		ctx11.drawImage(img, 0, 0, wImg, hImg, 0, 0, wCanvas, hCanvas);
+
+		let noside = di[k];
+
+		let xStart = type == 'war'?20:0;
+
+		let left = noside == 'left' ? 0 : findLeftLine(ctx1, wCanvas, hCanvas, cgoal,xStart); console.log('left', left)
+		let right = noside == 'right' ? wCanvas : findRightLine(ctx1, wCanvas, hCanvas, cgoal); console.log('right', right)
+		let top = noside == 'top' ? 0 : findTopLine(ctx1, wCanvas, hCanvas, cgoal); console.log('top', top)
+		let bot = noside == 'bottom'||type=='war' ? hCanvas : findBottomLine(ctx1, wCanvas, hCanvas, cgoal); console.log('bot', bot)
+		let [x1, x2, y1, y2, dx, dy, factw, facth] = [left, right, top, bot, 8, 8, 2, 2];
+
+		if (k == 'hawaii') {dx=16; factw=1.2}
+		else if (k.includes('antiky')) {dx=16; factw=1.1; dy=10;}
+		ctx.drawImage(cv1, x1, y1, x2 - x1, y2 - y1, dx, dy, w - factw * dx, h - facth * dy);
+		let diColors = { advisor: 'orange', battle: 'grey', building: 'deepskyblue', colony: 'green', event: 'purple', golden_age: 'gold', military: 'red', war: 'black', natural: 'maroon', wonder: 'sienna' };
+		ctx.strokeStyle = diColors[card.Type];
+		ctx.lineWidth = 28;
+		ctx.strokeRect(0, 0, w, h);
+		await imgToServer(canvas, `y/nat/${type}/${k}.png`);
+
+	}
+	function findLeftLine(ct, w, h, cgoal, xStart=0) {
+
+		let [restlist, _] = findPointsBoth(ct, xStart, xStart+40, 0, h, cgoal, 20);
 		let o = nextBar(ct, restlist, 'red');
 		return o.val;
 	}
@@ -431,7 +504,7 @@ async function test64_finalize(type) {
 		military: { diffleft: 176, diffright: 63, dx: 250, y: 240, xmin: 180, top: 176, bot: 67 }
 	};
 	for (const k of list) {
-		if (['antikythera_mechanism','uluru','mount_kailash','hawaii','great_barrier_reef','uraniborg', 'terracotta', 'pyramids', 'hagia', 'kremlin', 'potemkin'].some(x => k.includes(x))) continue;
+		if (['second_boer_war','opium_war','balkan_wars','antikythera_mechanism','uluru','mount_kailash','hawaii','great_barrier_reef','uraniborg', 'terracotta', 'pyramids', 'hagia', 'kremlin', 'potemkin'].some(x => k.includes(x))) continue;
 		console.log('____________', k)
 		let [canvas, ctx, w, h] = await natGetEmptyCardCanvas(dParentGood);
 		let card = M.natCards[k];
@@ -522,8 +595,8 @@ async function test62_points() {
 async function test61_lines() {
 	M.natCards = await mGetYaml('../assets/games/nations/cards.yaml');
 	let list = rChoose(Object.keys(M.natCards).filter(ck => M.natCards[ck].Type != 'event'), 6);
-	list = Object.keys(M.natCards).filter(ck => M.natCards[ck].Type == 'war' && M.natCards[ck].age > 0)
-	//list = ['great_barrier_reef']; // brewery aqueduct
+	list = Object.keys(M.natCards).filter(ck => M.natCards[ck].Type == 'battle' && M.natCards[ck].age > 0)
+	//list = ['second_boer_war']; // brewery aqueduct
 	// elephant anna_komnene abu_bakr aeneid hanging_gardens solomons_temple archer university hoplite great_northern_war
 	// list = ['abu_bakr', 'great_northern_war','hoplite','aeneid','university','archer','solomons_temple','hanging_gardens'];// aeneid hanging_gardens solomons_temple archer university hoplite great_northern_war
 	let dParentGood = toElem('dExtra');
@@ -541,7 +614,7 @@ async function test61_lines() {
 		military: { diffleft: 176, diffright: 63, dx: 250, y: 240, xmin: 180, top: 176, bot: 67 }
 	};
 	for (const k of list) {
-		if (['antiky','uluru','mount_kailash','hawaii','great_barrier_reef','uraniborg', 'terracotta', 'pyramids', 'hagia', 'kremlin', 'potemkin'].some(x => k.includes(x))) continue;
+		if (['second_boer_war','opium_war','balkan_wars','antiky','uluru','mount_kailash','hawaii','great_barrier_reef','uraniborg', 'terracotta', 'pyramids', 'hagia', 'kremlin', 'potemkin'].some(x => k.includes(x))) continue;
 		console.log('____________', k)
 		let [canvas, ctx, w, h] = await natGetEmptyCardCanvas(dParentGood);
 		let card = M.natCards[k];
