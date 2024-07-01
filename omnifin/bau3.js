6eaf39524a924d10f07de4b409bf106dac63e3ce
isawzz <iwgajam@gmail.com>
2024-07-01 10:18:09 -0700

    das

--- omnifin/bau3.js
+++ omnifin/bau3.js
@@ -9,6 +9,9 @@ function dbtable(q,dParent){
 	let result = dbq(q)[0];
 
 	let t=mTable(dParent, result.columns, true);
+	for(const rec of result.values){
+		console.log(rec); return;
+	}
 
 	// return output = result.map(({ columns, values }) => {
 	// 	return columns.join('\t') + '\n' + values.map(row => row.join('\t')).join('\n');
