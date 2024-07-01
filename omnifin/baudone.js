a5f4719d383a7a9b76ad10df9e5859efc819fd51
isawzz <iwgajam@gmail.com>
2024-07-01 07:23:52 -0700

    das

--- omnifin/baudone.js
+++ omnifin/baudone.js
@@ -15,3 +15,14 @@ async function dbInit(path) {
 	}
 
 }
+function showNavbar() {
+	mDom('dNav',{fz:34,mabottom:10,w100:true},{html:`Omnifin`});
+	let nav = mMenu('dNav');
+	let commands = {};
+	commands.home = menuCommand(nav.l, 'nav', 'home', 'Overview', menuOpenOverview, menuCloseHome);
+	commands.sql = menuCommand(nav.l, 'nav', 'sql', 'Sql', menuOpenSql, menuCloseSql);
+	commands.tables = menuCommand(nav.l, 'nav', 'tables', 'Tables', menuOpenTables, menuCloseTables);
+	commands.test = menuCommand(nav.l, 'nav', 'test', 'Test', menuOpenTest, menuCloseTest);
+	nav.commands = commands;
+	return nav;
+}
--- omnifin/bausammel.js
+++ omnifin/bausammel.js
@@ -19,20 +19,6 @@ function dbQuery(db, q, dParent) {
 
 }
 
-function showNavbar() {
-	mDom('dNav',{fz:34,mabottom:10,w100:true},{html:`Omnifin`});
-	let nav = mMenu('dNav');
-	let commands = {};
-	commands.home = menuCommand(nav.l, 'nav', 'home', 'Overview', menuOpenOverview, menuCloseHome);
-	commands.sql = menuCommand(nav.l, 'nav', 'sql', 'Sql', menuOpenSql, menuCloseSql);
-	commands.tables = menuCommand(nav.l, 'nav', 'tables', 'Tables', menuOpenTables, menuCloseTables);
-	nav.commands = commands;
-	return nav;
-}
-async function menuOpenOverview(){
-	// sidebarHome();
-	// mDom('dMain',{wmax:'1000'},{tag:'pre',id:'dPre'});
-}
 function sidebarHome() {
 	let wmin = 170;
 	mStyle('dLeft', { wmin: wmin });
@@ -41,24 +27,6 @@ function sidebarHome() {
 	UI.commands.showSchema = mCommand(d, 'showSchema', 'DB Structure'); mNewline(d, gap);
 }
 async function onclickSql(){console.log('hallo')}
-async function menuOpenSql(){
-	// mClear('dMain')
-	sidebarSql();
-	// let d=mDom('dMain',{w:window.innerWidth-200,box:true});
-	// UI.ta=mDom(d,{w100:true,box:true},{tag:'textarea',id:'taSql',rows:10,value:'select * from transactions'});
-	// mButton('Execute',onclickExecute,d,{},'button');
-
-	// let d1 = mDom(d,{wmax:1000},{tag:'pre',bg:'silver',overflow:'hidden',id:'dPre'}); return;
-	// let text = dbQuery(DB,UI.ta.value);
-
-	// // mTable(d1)
-	// // const output = res.map(({ columns, values }) => {
-	// // 	return columns.join('\t') + '\n' + values.map(row => row.join('\t')).join('\n');
-	// // }).join('\n\n');
-
-	// d1.textContent = text; //output;
-	// // mDom(d1,{overx:'auto'},{tag:'pre',id:'dPre'});
-}
 async function onclickExecute(){
 	dbQuery(DB,UI.ta.value,'dPre');
 }
@@ -69,11 +37,6 @@ function sidebarSql() {
 	let gap = 5;
 	//UI.commands.showSchema = mCommand(d, 'showSchema', 'DB Structure'); mNewline(d, gap);
 }
-
-async function menuCloseHome(){closeLeftSidebar();mClear('dMain')}
-async function menuCloseSql(){closeLeftSidebar();mClear('dMain')}
-async function menuOpenTables(){}
-async function menuCloseTables(){closeLeftSidebar();mClear('dMain')}
 
 //stubs
 async function updateExtra(){}
