26749e7e4d7b95fad9a0ad8ef97aa9a2174dd53c
isawzz <iwgajam@gmail.com>
2024-07-28 15:57:17 -0700

    das

--- wiki/start.js
+++ wiki/start.js
@@ -1,69 +1,8 @@
 onload=start;
 
-async function start() { await test2_citiesKovalam(); }
+async function start() { await test1(); }
 
-async function test2_citiesKovalam(){
-  let di = await mGetYaml('../wikisaves/di.yaml');
-  console.log(di['9245'],di['9246']);
-}
-async function test2_articles(){
-  let di = await mGetYaml('../wikisaves/di.yaml');
-  console.log(Object.keys(di).length);
-  let cities = {},byTitle={};
-  for(const k in di){
-    let o = di[k];
-    let cats = o.cats;
-    assertion(isList(cats),`problem! ${k}`);
-    if (cats.some(x=>x.toLowerCase().includes('city'))) cities[k]=o;
-  }
-  console.log(Object.values(cities).map(x=>x.title));
-}
-async function test2_zusammen(){
-  let dir='../wikisaves/filenofile80';
-  let files=await mGetFiles(dir,3001);
-  files.sort();
-  let di={},nodi={};
-  for(const f of files){
-    let o=await mGetYaml(`${dir}/${f}`);
-    if (f.startsWith('file')) addKeys(o,di); else break;//addKeys(o,nodi);
-  }
-  downloadAsYaml(di,'di');
-
-}
-async function test1_quoteFrom(){
-  let d=clearFlex();
-  let title='Johann Sebastian Bach'; //'Wolfgang Amadeus Mozart';
-  let result = await mGetRoute(`quotes/${encodeURIComponent(title)}`,null,3001);
-  console.log('was',result);
-  d.innerHTML = result;
-
-  let q=[];
-  let lis=Array.from(d.querySelectorAll('li'));
-  lis = Array.from(document.querySelectorAll('div>ul>li'));  
-  console.log(lis);
-  for(const li of lis){
-    let text = li.textContent;
-    let parts = splitAtAnyOf(text,'\n(')
-    q.push(parts[0]); 
-  }
-  console.log(q)
-
-  
-}
-async function test1_quote(){
-  clearFlex();
-  let url = 'http://localhost:3001/testquote';
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-    mode: 'cors',
-  });
-
-  let res = tryJSONParse(await response.text());
-  console.log('was',res);
-
-  
-}
+async function test1(){}
 async function test0(){
 
   //console.log('hhhhhhhhhhhhhhh'); return;
